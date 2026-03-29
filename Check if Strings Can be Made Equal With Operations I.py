d34f07b1b2dd24ebd0a10e90c083b60c5e0e489a
Pranay Shridhar <62248912+pranay2704@users.noreply.github.com>
2026-03-29 18:42:18 +0530

    Create Check if Strings Can be Made Equal With Operations I.py

---
+++ Check if Strings Can be Made Equal With Operations I.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        lt1,lt2,rt1,rt2 = [],[],[],[]
+        for i in range(len(s1)):
+            if i%2==0:
+                lt1.append(s1[i])
+                lt2.append(s2[i])
+            else:
+                rt1.append(s1[i])
+                rt2.append(s2[i])
+        if sorted(lt1)!=sorted(lt2) or sorted(rt1)!=sorted(rt2):
+            return False
+        return True
