bef19f6216ac2c4979044723c2c3b9ee81b017e4
Pranay Shridhar <62248912+pranay2704@users.noreply.github.com>
2026-04-06 15:38:48 +0530

    Create Top K Frequent Elements.py

---
+++ Top K Frequent Elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = {}
+        for i in nums:
+            if i in dic.keys():
+                dic[i]+=1
+            else:
+                dic[i]=1
+        dic = dict(sorted(dic.items(),key = lambda x:x[1],  reverse=True))
+        res=[]
+        for i,j in dic.items():
+            if k>0:
+                res.append(i)
+                k-=1
+        return res
