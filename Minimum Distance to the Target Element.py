67a31a88b66125874578a1b13e65d0b76f9c11f1
Pranay Shridhar <62248912+pranay2704@users.noreply.github.com>
2026-04-13 20:33:12 +0530

    Create Minimum Distance to the Target Element.py

---
+++ Minimum Distance to the Target Element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getMinDistance(self, nums, target, start):
+        min_dist = float('inf')
+        for i in range(len(nums)):
+            if nums[i] == target:
+                min_dist = min(min_dist, abs(i - start))
+        return min_dist
