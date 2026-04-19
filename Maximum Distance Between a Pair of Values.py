4b02cec6f35da9e5f2229ed2df2e57a321582ec1
Pranay Shridhar <62248912+pranay2704@users.noreply.github.com>
2026-04-19 11:58:29 +0530

    Update Maximum Distance Between a Pair of Values.py

--- Maximum Distance Between a Pair of Values.py
+++ Maximum Distance Between a Pair of Values.py
@@ -3,14 +3,10 @@ class Solution:
         dist=0
         i=len(nums1)-1
         j=len(nums2)-1
-        while i>-1:
-            # print(nums1[i], nums2[0])
-            if nums1[i]>nums2[0]:
-                break
+        while i>-1 and nums1[i]<=nums2[0]:
             while j>0 and j>=i and nums1[i]>nums2[j]:
                 j-=1          
             if j!=-1:
                 dist=max(dist,j-i)
-            # print(i,j,dist)
             i-=1
         return dist
